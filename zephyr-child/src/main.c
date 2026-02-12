92951bdac8c40572b2c1782a4ba1e0c8455e7750
kurtS <kurt.florian.schrammel@proton.me>
2026-02-12 23:04:41 +0100

    feat: distinction between cumulative and hard sync, updated logging

--- zephyr-child/src/main.c
+++ zephyr-child/src/main.c
@@ -14,17 +14,21 @@ static const struct gpio_dt_spec sync_pin = GPIO_DT_SPEC_GET(SYNC_PIN_NODE, gpio
 static struct gpio_callback sync_cb_data;
 static struct k_sem sync_sem;
 
+/* Time latched during hardware interrupt */
 static volatile uint64_t latched_local_us = 0;
 static int64_t clock_offset_us = 0;
 
+/* Returns raw hardware uptime in microseconds */
 static inline uint64_t get_uptime_us(void) {
     return (k_cycle_get_64() * 1000000ULL) / sys_clock_hw_cycles_per_sec();
 }
 
+/* Returns uptime adjusted by the calculated clock offset */
 static inline uint64_t get_synced_uptime_us(void) {
     return (uint64_t)((int64_t)get_uptime_us() + clock_offset_us);
 }
 
+/* ISR: Executed immediately on hardware signal edge */
 void sync_callback(const struct device *port, struct gpio_callback *cb, uint32_t pins) {
     latched_local_us = get_uptime_us();
     k_sem_give(&sync_sem);
@@ -45,6 +49,7 @@ static const struct spi_config slave_cfg = {
 int main(void) {
     const struct device *spi_dev = DEVICE_DT_GET(SPI_DEV_NODE);
     
+    /* Initialize Semaphore and GPIO Interrupt */
     k_sem_init(&sync_sem, 0, 1);
     gpio_pin_configure_dt(&sync_pin, GPIO_INPUT);
     gpio_pin_interrupt_configure_dt(&sync_pin, GPIO_INT_EDGE_TO_ACTIVE);
@@ -53,31 +58,45 @@ int main(void) {
 
     int sync_count = 0;
     while (sync_count < 1000) {
-        printk("CHILD %d waiting for sync #%d...\n", CHILD_ID, sync_count + 1);
+        /* Block until Master sends hardware trigger pulse */
         k_sem_take(&sync_sem, K_FOREVER);
         
+        /* Receive Master Timestamp via SPI */
         if (spi_transceive(spi_dev, &slave_cfg, &tx_set, &rx_set) >= 0) {
             uint64_t master_ts_us = 0;
             for (int i = 0; i < 8; i++) master_ts_us = (master_ts_us << 8) | rx_data[i];
 
+            /* Calculate reference time at the exact moment of the interrupt */
             uint64_t local_ref_us = (sync_count == 0) ? 
                                     latched_local_us : 
                                     (uint64_t)((int64_t)latched_local_us + clock_offset_us);
             
             int64_t diff_us = (int64_t)master_ts_us - (int64_t)local_ref_us;
-            clock_offset_us += diff_us;
-            sync_count++;
 
+            /* TWO METHODS OF SYNCHRONIZATION */
+            #ifdef USE_CUMULATIVE_SYNC
+                /* Method A: Cumulative (Drift Compensation) 
+                   Adds error to current offset for gradual convergence */
+                clock_offset_us += diff_us;
+            #else
+                /* Method B: Hard Step (Absolute Set) 
+                   Resets offset based on the latest hardware latch */
+                clock_offset_us = (int64_t)master_ts_us - (int64_t)latched_local_us;
+            #endif
+
+            sync_count++;
+            
             double offset_ms = (double)diff_us / 1000.0;
             int32_t ms_int = (int32_t)offset_ms;
             uint32_t ms_frac = (uint32_t)(llabs((int64_t)((offset_ms - ms_int) * 1000000.0)));
+            
             double synced_uptime_ms = (double)get_synced_uptime_us() / 1000.0;
             uint32_t synced_ms_int = (uint32_t)synced_uptime_ms;
             uint32_t synced_ms_frac = (uint32_t)((synced_uptime_ms - synced_ms_int) * 1000000.0);
 
-            printk("CHILD %d clock offset: %s%d.%06u ms\n", 
-                   CHILD_ID, (offset_ms < 0 && ms_int == 0) ? "-" : "", ms_int, ms_frac);
-            printk("CHILD %d synced uptime: %u.%06u ms\n", CHILD_ID, synced_ms_int, synced_ms_frac);
+            printk("CHILD %d offset: %s%d.%06u ms | synced: %u.%06u ms\n", 
+                   CHILD_ID, (offset_ms < 0 && ms_int == 0) ? "-" : "", ms_int, ms_frac, 
+                   synced_ms_int, synced_ms_frac);
         }
     }
     return 0;
